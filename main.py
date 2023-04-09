6ff38cfa124fb7c6f53bc1f981b3ab3e4b186cf9
cheapdramas <bileckijrostislav46@gmail.com>
2023-04-09 16:59:47 +0300

    IDKD

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame.time
 from pygame import *
 from sys import exit
+from random import randint
+
 
 init()
-
+WIDTH = 800
+HEIGHT = 800
 window = pygame.display.set_mode((800,800))
+pygame.display.set_caption('Ping Pong')
+
 
 clock = pygame.time.Clock()
 
@@ -29,6 +34,32 @@ class Platform_2(pygame.Rect):
             self.y -= 5
         if dy == -5:
             self.y += 5
+    
+ball_obj = pygame.draw.circle(window,(255,255,255),(400,400),40)
+speed = 2
+
+class Ball(pygame.Rect):
+    def __init__(self,x,y,width,platform1,platform2,height):
+        super().__init__(x,y,width,height)
+        self.x = x
+        self.y = y
+        self.platform_1 = platform1
+        self.platform_2 = platform2
+        self.speed = 2
+
+        self.radius = 20
+    def draw(self):
+        pygame.draw.circle(window,(255,255,255),(self.x,self.y),self.radius)
+    
+   
+
+
+
+
+
+
+
+
 
 
 
@@ -38,6 +69,21 @@ class Platform_2(pygame.Rect):
 
 platform_1 = Platform_1(0,400,50,50)
 platform_2 = Platform_2(750,400,50,50)
+ball = Ball(400,400,50,platform_1,platform_2,50)
+r = randint(1,100)
+random_move = True
+ball_speed_x = 7
+ball_speed_y = 7
+
+if r <= 50:
+    ball_speed_x = -7
+    ball_speed_y = -7
+if r >= 50:
+    ball_speed_x = 7
+    ball_speed_y = 7
+
+    
+
 
 
 while True:
@@ -46,10 +92,29 @@ while True:
             exit()
     window.fill((0,0,0))
     keys = pygame.key.get_pressed()
+    
     if keys[K_s]:
         platform_1.move(-5)
     if keys[K_w]:
         platform_1.move(5)
+    
+    ball.x += ball_speed_x 
+    ball.y += ball_speed_y
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_speed_y *= -1
+        
+    if ball.left <= 0 or ball.right >= WIDTH:
+        ball_speed_x *= -1  
+    
+    if ball.colliderect(platform_1) or ball.colliderect(platform_2):
+        ball_speed_x *= -1
+
+     
+    
+        
+    ball.draw()
+    
+   
 
     
     
@@ -61,6 +126,9 @@ while True:
 
     window.blit(platform_1.IMAGE,(platform_1.x,platform_1.y))
     window.blit(platform_2.IMAGE,(platform_2.x,platform_2.y))
+    
+    
+    
     
     
 
